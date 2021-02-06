acf5d44c90ceee696dd84683a83bac053ab6819b
mohamedfarid <mhmdmedo820@gmail.com>
2021-02-06 23:48:54 +0200

    init css & add custom range input

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function setSlider() {
+  const slider = document.querySelector("#CustomSlider");
+  const max = slider.getAttribute("max");
+  const width = getComputedStyle(slider).getPropertyValue("--width");
+  const widthValue = Number(width.slice(0, width.length - 2));
+
+  const minimumFillWidth = widthValue * 0.03;
+  const maximumFillWidthRatio = 0.95;
+
+  function setFilledArea() {
+    const fillRation = slider.value / max;
+    let fillWidth = fillRation * widthValue * maximumFillWidthRatio;
+    if (fillWidth < minimumFillWidth) fillWidth = 0;
+    slider.style.setProperty("--value", fillWidth + "px");
+  }
+  setFilledArea();
+  slider.addEventListener("input", setFilledArea);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setSlider();
+});
