19bf3771df5c704b129b50c41ca13e11b7a32968
mohamedfarid <mhmdmedo820@gmail.com>
2021-02-10 22:11:45 +0200

    finish desktop version

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function setSlider() {
-  const slider = document.querySelector("#CustomSlider");
+  const slider = document.querySelector(".CustomSlider");
   const max = slider.getAttribute("max");
   const width = getComputedStyle(slider).getPropertyValue("--width");
-  const widthValue = Number(width.slice(0, width.length - 2));
+  const widthValue = Number(width.slice(0, width.length - 1));
 
   const minimumFillWidth = widthValue * 0.03;
   const maximumFillWidthRatio = 0.95;
@@ -11,7 +11,7 @@ function setSlider() {
     const fillRation = slider.value / max;
     let fillWidth = fillRation * widthValue * maximumFillWidthRatio;
     if (fillWidth < minimumFillWidth) fillWidth = 0;
-    slider.style.setProperty("--value", fillWidth + "px");
+    slider.style.setProperty("--value", fillWidth + "%");
   }
   setFilledArea();
   slider.addEventListener("input", setFilledArea);
